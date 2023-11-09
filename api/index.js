13382f7a333d91792403fd8f6367089cb358bad8
Divyansh Srivastavae <divyanshsrivastava215@gmail.com>
2023-11-09 16:10:41 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("database connected successfully");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
-
-app.listen(3000,() => {
-    console.log("server is running on 3000!!!")
-})
+app.listen(3000, () => {
+  console.log("server is running on 3000!!!");
+});
